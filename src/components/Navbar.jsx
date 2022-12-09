3bfdd06c0704b2be0c12616b913eb952d3cbbebc
Rodrigo Alarcón <rodalarcon@gmail.com>
2022-12-09 12:16:52 -0300

    Configuracion del proyecto y el layout

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap';
+const Navbar = () => {
+  return (
+    <>
+    <Row>
+        <Col>
+        
+        </Col>
+
+        <Col>
+        
+        </Col>
+    </Row>
+    </>
+  )
+}
+
+export default Navbar;
